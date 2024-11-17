c7020bbabe36a6802ca306aa68e193b6543714f7
anischenko alexander <a.anischenko.de@gmail.com>
2024-11-17 19:22:57 +0100

    HomeWork03 change Prettier

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -7,10 +7,22 @@ function LoginForm() {
         <form className="loginform-wrapper" action="">
             <div className="loginform-name">Login form</div>
             <div className="loginform-input">
-                <Input label= 'Email' placeholder='Enter your email' type='email' name='userEmail' id='email-id'/>
-                <Input label='Password' placeholder='Enter your password' type='password' name='userPassword' id='password-id'/>
+                <Input
+                    label="Email"
+                    placeholder="Enter your email"
+                    type="email"
+                    name="userEmail"
+                    id="email-id"
+                />
+                <Input
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    name="userPassword"
+                    id="password-id"
+                />
             </div>
-            <Button buttonName='Login'/>
+            <Button buttonName="Login" />
         </form>
     );
 }
